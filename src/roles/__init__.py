92b9f372e53694eca83c8a5ea43b5b0a9c9d347e
yifan-lee <ivanlee142857@gmail.com>
2026-02-16 21:31:06 -0500

    太复杂了，新branch开始简化

--- src/roles/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base_role import BaseRole, VillagerRole
-from .wolf_role import WolfRole
-from .seer_role import SeerRole
-from .witch_role import WitchRole
-from .hunter_role import HunterRole
-from .idiot_role import IdiotRole
